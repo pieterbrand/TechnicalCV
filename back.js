65f0a5afc5dda7456ddbf0b5687fba797da6a7ea
pieterbrand <37172613+pieterbrand@users.noreply.github.com>
2020-10-12 18:53:41 +0200

    Commit alpha

--- back.js
+++ back.js
@@ -1,6 +1,16 @@
 var title = new Vue({
     el: '#title',
+    
     data:{
-        name: "Pieter Johannes Brand"
+        name: "Pieter Johannes Brand",
+        birthday: "1998-10-01",
+        place_birth: "Kimberley",
+        primarysc: "Eureka Primary School",
+        secondsc: "Diamantveld Highschool",
+        university: "North West Univerity (Potchefstroom)",
+        qualification1: "NSC Matric 2016", 
+        qualification2: "Bachelor of Science in Information Technology (BSc IT) expected graduation: 2021.",
+        email: "pbrand61@gmail.com",
+        cell: "+2783 271 1102"
     }
 })
